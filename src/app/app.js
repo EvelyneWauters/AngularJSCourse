c9fa7c1058c1a651f3001305d3dc624b926bae65
EvelyneWauters <evelyne.wauters@gmail.com>
2015-06-26 11:43:04 +0200

    app.js added config & run function

--- src/app/app.js
+++ src/app/app.js
@@ -1,1 +1,11 @@
-angular.module('movieApp', []);
+var app = angular.module('movieApp', []);
+
+
+app.config(function()   {
+   console.log('app config');
+});
+
+app.run(function()  {
+    console.log('app run');
+
+});
